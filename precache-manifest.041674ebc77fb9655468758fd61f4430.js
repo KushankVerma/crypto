3667e3d4b2632f8de80a17cfae91bbc14a49428b
Kushank Verma <kushankverma934@gmail.com>
2022-02-17 01:04:59 +0530

    Updates

---
+++ precache-manifest.041674ebc77fb9655468758fd61f4430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1563d99ab81e190494a1414f4ea97514",
+    "url": "/crypto/index.html"
+  },
+  {
+    "revision": "a08907b878ca5259c25a",
+    "url": "/crypto/static/css/main.2b07b6aa.chunk.css"
+  },
+  {
+    "revision": "af2cc4927ee53754f4c8",
+    "url": "/crypto/static/js/2.29edc68e.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/crypto/static/js/2.29edc68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08907b878ca5259c25a",
+    "url": "/crypto/static/js/main.d613c9c0.chunk.js"
+  },
+  {
+    "revision": "e5262e904e0eaee38760",
+    "url": "/crypto/static/js/runtime-main.9f9f90ca.js"
+  }
+]);
